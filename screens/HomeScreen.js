b705813dedd7bf75eac8e4aadcd175e1ce4c4267
Beom Mee <whitebigbabybird@gmail.com>
2023-12-23 19:44:00 +0900

    set SafeAreaView and navigation header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, SafeAreaView } from 'react-native' // [important] import SafeAreaView from react-native for low import cost
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    /** set the layout without the header zone */
+    navigation.setOptions({ 
+      // headerTitle: 'testing',
+      headerShown: false,
+    })
+  }, []);
+
   return (
-    <View>
-      <Text className='text-red-700'>HomeScreen</Text>
-    </View>
+    /** you can put things inside the safe area no matter in which device users are using */
+    <SafeAreaView>
+      <Text className='text-red-500'>HomeScreen</Text>
+    </SafeAreaView>
   )
 }
