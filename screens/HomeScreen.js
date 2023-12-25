f828994a01ad288dda14f957d8222fb596d442ec
Beom Mee <whitebigbabybird@gmail.com>
2023-12-25 20:09:31 +0900

    Add Categories section and change keyboardtype into inputmode

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import { Text, SafeAreaView, View, Image, TextInput } from 'react-native' // [important] import SafeAreaView from react-native for low import cost
+import { Text, SafeAreaView, View, Image, TextInput, ScrollView } from 'react-native' // [important] import SafeAreaView from react-native for low import cost
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { ChevronDownIcon, UserIcon, MagnifyingGlassIcon, AdjustmentsVerticalIcon } from 'react-native-heroicons/outline'
+import Categories from '../components/categories';
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -42,10 +43,18 @@ export default function HomeScreen() {
       <View className='flex-row items-center space-x-2 pb-2 mx-4'>
         <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
           <MagnifyingGlassIcon size={20} color='gray' />
-          <TextInput placeholder='Restaurants and Cuisines' keyboardType='default' />
+          <TextInput placeholder='Restaurants and Cuisines' inputMode='default' />
         </View>
         <AdjustmentsVerticalIcon size={20} color='#00ccbb' />
       </View>
+
+      {/* Body */}
+      <ScrollView className='bg-gray-100' contentContainerStyle={{ paddingBottom: 100 }}>
+        {/* Categories */}
+        <Categories />
+
+        {/* Featured Rows */}
+      </ScrollView>
     </SafeAreaView>
   )
 }
