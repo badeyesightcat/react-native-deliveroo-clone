2df1dc605f9b8872d8b2d9d7b4f777a652ebaa11
Beom Mee <whitebigbabybird@gmail.com>
2023-12-30 18:14:33 +0900

    Add RestaurantCard

---
+++ components/RestaurantCard.js
@@ -0,0 +1,56 @@
+import { View, Text, Pressable, Image } from "react-native";
+import React from "react";
+import { StarIcon } from "react-native-heroicons/solid";
+import { MapPinIcon } from "react-native-heroicons/outline";
+
+const RestaurantCard = ({
+  id,
+  imgUrl,
+  title,
+  rating,
+  genre,
+  address,
+  short_description,
+  dishes,
+  long,
+  lat,
+}) => {
+  return (
+    <Pressable className="bg-white mr-3 shadow">
+      <Image
+        source={{
+          uri: imgUrl,
+        }}
+        className="h-36 w-64 rounded-sm"
+      />
+
+      <View className="px-3 pb-4">
+        <Text className="font-bold text-lg pt-2">{title}</Text>
+
+        <View className="flex-row items-center space-x-1">
+          <StarIcon
+            color="green"
+            opacity={0.5}
+            size={20}
+          />
+          <Text className="text-xs text-gray-500">
+            <Text className="text-green-500">{rating}</Text> &middot; {genre}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center space-x-1">
+          <MapPinIcon
+            color="gray"
+            opacity={0.4}
+            size={20}
+          />
+          <Text className="text-xs text-gray-500">
+            Nearby &middot; {address}
+          </Text>
+        </View>
+      </View>
+    </Pressable>
+  );
+};
+
+export default RestaurantCard;
