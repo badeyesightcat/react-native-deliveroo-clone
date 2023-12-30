db388ec89ae0eb305a4d0af29869a3060317322c
Beom Mee <whitebigbabybird@gmail.com>
2023-12-30 18:15:07 +0900

    Use RestaurantCard for FeaturedRow

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,6 +1,7 @@
 import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { ArrowRightIcon } from "react-native-heroicons/outline";
+import RestaurantCard from "./RestaurantCard";
 
 const FeaturedRow = ({ title, description, id, featuredCategory }) => {
   return (
@@ -21,7 +22,44 @@ const FeaturedRow = ({ title, description, id, featuredCategory }) => {
         className="pt-4"
       >
         {/* RestaurantCards... */}
-        <RestaurantCard></RestaurantCard>
+        <RestaurantCard
+          id={123}
+          imgUrl="https://images.unsplash.com/photo-1597699401474-e8714c1b7879?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          title="Yum bakeries"
+          rating={4.5}
+          genre="Bakeries"
+          address="123 Main St"
+          short_description="This is short description message for testing"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+
+        <RestaurantCard
+          id={123}
+          imgUrl="https://images.unsplash.com/photo-1597699401474-e8714c1b7879?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          title="Yum bakeries"
+          rating={4.5}
+          genre="Bakeries"
+          address="123 Main St"
+          short_description="This is short description message for testing"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+
+        <RestaurantCard
+          id={123}
+          imgUrl="https://images.unsplash.com/photo-1597699401474-e8714c1b7879?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          title="Yum bakeries"
+          rating={4.5}
+          genre="Bakeries"
+          address="123 Main St"
+          short_description="This is short description message for testing"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
       </ScrollView>
     </View>
   );
