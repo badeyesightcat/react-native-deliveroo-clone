24e5e30114381eaa022c386cd0203ec3e42a7c7d
Beom Mee <whitebigbabybird@gmail.com>
2024-01-01 20:16:21 +0900

    Apply prettier

--- sanity/sanity.cli.js
+++ sanity/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '5sg3p461',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
