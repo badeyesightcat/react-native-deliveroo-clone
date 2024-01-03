c9ddf27fbc31c0d2a79592021063155af78ac4ee
Beom Mee <whitebigbabybird@gmail.com>
2024-01-03 14:00:23 +0900

    feat: add restaurantScreen hero image and goback button: 1) set header disappeared 2) link onPress event to the go-back button

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,6 +1,8 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { useRoute } from "@react-navigation/native";
+import { Pressable, ScrollView, View, Text, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { urlFor } from "../sanity";
+import { ArrowLeftIcon } from "react-native-heroicons/solid";
 
 const RestaurantScreen = () => {
   // tip: Use destructured params
@@ -18,11 +20,37 @@ const RestaurantScreen = () => {
       lat,
     },
   } = useRoute();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <View>
+    <ScrollView>
+      <View className="relative">
+        <Image
+          source={{
+            uri: urlFor(imgUrl).url(),
+          }}
+          className="w-full h-56 bg-gray-300 p-4"
+        />
+
+        <Pressable
+          onPress={navigation.goBack}
+          className="absolute top-14 left--5 p-2 bg-gray-100 rounded-full"
+        >
+          <ArrowLeftIcon
+            size={20}
+            color="#00ccbb"
+          />
+        </Pressable>
+      </View>
+
       <Text>{title}</Text>
-    </View>
+    </ScrollView>
   );
 };
 
