834b38fd43da1e93d04124d3ec4ab688a4a852e2
Beom Mee <whitebigbabybird@gmail.com>
2024-01-03 11:14:06 +0900

    feat: add restaurant screen to App.js and format codes according to prettier

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import RestaurantScreen from "./screens/RestaurantScreen";
 
-import './style.css'
+import "./style.css";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +11,14 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Restaurant"
+          component={RestaurantScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
