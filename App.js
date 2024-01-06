57c0d3d23ce1533b4b056826056514cda77ec761
Beom Mee <whitebigbabybird@gmail.com>
2024-01-06 20:33:12 +0900

    feat(store): add store to App

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
+// import { Linking, Platform } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 import "./style.css";
 
@@ -10,16 +13,18 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
-        <Stack.Screen
-          name="Restaurant"
-          component={RestaurantScreen}
-        />
-      </Stack.Navigator>
+      <Provider store={store}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            name="Restaurant"
+            component={RestaurantScreen}
+          />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
