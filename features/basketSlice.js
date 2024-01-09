c1dc4ded897b1400776f47ec1a5132ca370fad8a
Beom Mee <whitebigbabybird@gmail.com>
2024-01-09 17:28:24 +0900

    feat: define remove-item-from-basket reducer function

--- features/basketSlice.js
+++ features/basketSlice.js
@@ -11,7 +11,23 @@ export const basketSlice = createSlice({
     addToBasket: (state, action) => {
       state.items = [...state.items, action.payload];
     },
-    removeFromBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {
+      const sameIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      const newBasket = [...state.items];
+
+      if (sameIndex >= 0) {
+        newBasket.splice(sameIndex, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.payload.id}) as it is not in basket. `
+        );
+      }
+
+      state.items = newBasket;
+    },
   },
 });
 
