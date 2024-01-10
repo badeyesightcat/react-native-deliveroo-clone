82bbd5f2b0e8ad5087941e94aa78ea51bb13cb62
Beom Mee <whitebigbabybird@gmail.com>
2024-01-10 14:23:53 +0900

    feat: define selectBasketTotal method be careful to use reduce method

--- features/basketSlice.js
+++ features/basketSlice.js
@@ -35,4 +35,7 @@ export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 export const selectBasketItems = (state) => state.basket.items;
 
+export const selectBasketTotal = (state) =>
+  state.basket.items.reduce((total, item) => total + item.price, 0);
+
 export default basketSlice.reducer;
