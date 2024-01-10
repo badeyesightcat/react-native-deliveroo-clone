94e3474faadcf86f40a725ea96b1150e6520b239
Beom Mee <whitebigbabybird@gmail.com>
2024-01-10 17:49:32 +0900

    feat: add BasketScreen with options defined

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
+import BasketScreen from "./screens/BasketScreen";
 // import { Linking, Platform } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
@@ -22,6 +23,14 @@ export default function App() {
           <Stack.Screen
             name="Restaurant"
             component={RestaurantScreen}
+          />
+          <Stack.Screen
+            name="Basket"
+            component={BasketScreen}
+            options={{
+              presentation: "modal",
+              // headerShown: false,
+            }}
           />
         </Stack.Navigator>
       </Provider>
