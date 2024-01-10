36e6662c8434b60aa3ba2f021b08129d65dd0954
Beom Mee <whitebigbabybird@gmail.com>
2024-01-10 14:25:00 +0900

    feat: add BasketIcon component to RestaurantScreen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -10,6 +10,8 @@ import {
 } from "react-native-heroicons/solid";
 import { QuestionMarkCircleIcon } from "react-native-heroicons/outline";
 import DishRow from "../components/DishRow";
+import BasketIcon from "../components/BasketIcon";
+
 const RestaurantScreen = () => {
   // tip: Use destructured params
   const {
@@ -36,87 +38,90 @@ const RestaurantScreen = () => {
   }, []);
 
   return (
-    <ScrollView>
-      <View className="relative">
-        <Image
-          source={{
-            uri: urlFor(imgUrl).url(),
-          }}
-          className="w-full h-56 bg-gray-300 p-4"
-        />
-
-        <Pressable
-          onPress={navigation.goBack}
-          className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
-        >
-          <ArrowLeftIcon
-            size={20}
-            color="#00ccbb"
+    <>
+      <BasketIcon />
+      <ScrollView>
+        <View className="relative">
+          <Image
+            source={{
+              uri: urlFor(imgUrl).url(),
+            }}
+            className="w-full h-56 bg-gray-300 p-4"
           />
-        </Pressable>
-      </View>
 
-      <View className="bg-white">
-        <View className="px-4 pt-4">
-          <Text className="text-3xl font-bold">{title}</Text>
-
-          <View className="flex-row space-x-2 my-1">
-            <View className="flex-row items-center space-x-1">
-              <StarIcon
-                size={22}
-                color="green"
-                opacity={0.5}
-              />
-              <Text className="text-xs text-gray-500">
-                <Text className="text-green-500">{rating}</Text>
-                &middot; {genre}
-              </Text>
-            </View>
-
-            <View className="flex-row items-center space-x-1">
-              <MapPinIcon
-                size={22}
-                color="gray"
-                opacity={0.4}
-              />
-              <Text className="text-xs text-gray-500">
-                Nearby &middot; {address}
-              </Text>
-            </View>
-          </View>
-
-          <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+          <Pressable
+            onPress={navigation.goBack}
+            className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
+          >
+            <ArrowLeftIcon
+              size={20}
+              color="#00ccbb"
+            />
+          </Pressable>
         </View>
 
-        <Pressable className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
-          <QuestionMarkCircleIcon
-            color="gray"
-            opacity={0.6}
-            size={20}
-          />
-          <Text className="pl-2 flex-1 text-md font-bold">
-            Have a food allergy?
-          </Text>
-          <ChevronRightIcon color="#00ccbb" />
-        </Pressable>
-      </View>
+        <View className="bg-white">
+          <View className="px-4 pt-4">
+            <Text className="text-3xl font-bold">{title}</Text>
 
-      <View>
-        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+            <View className="flex-row space-x-2 my-1">
+              <View className="flex-row items-center space-x-1">
+                <StarIcon
+                  size={22}
+                  color="green"
+                  opacity={0.5}
+                />
+                <Text className="text-xs text-gray-500">
+                  <Text className="text-green-500">{rating}</Text>
+                  &middot; {genre}
+                </Text>
+              </View>
 
-        {/* DishRows */}
-        {dishes?.map((dish) => (
-          <DishRow
-            key={dish._id}
-            id={dish._id}
-            name={dish.name}
-            description={dish.short_description}
-            price={dish.price}
-            image={dish.image}
-          />
-        ))}
-      </View>
-    </ScrollView>
+              <View className="flex-row items-center space-x-1">
+                <MapPinIcon
+                  size={22}
+                  color="gray"
+                  opacity={0.4}
+                />
+                <Text className="text-xs text-gray-500">
+                  Nearby &middot; {address}
+                </Text>
+              </View>
+            </View>
+
+            <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+          </View>
+
+          <Pressable className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+            <QuestionMarkCircleIcon
+              color="gray"
+              opacity={0.6}
+              size={20}
+            />
+            <Text className="pl-2 flex-1 text-md font-bold">
+              Have a food allergy?
+            </Text>
+            <ChevronRightIcon color="#00ccbb" />
+          </Pressable>
+        </View>
+
+        <View>
+          <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+
+          {/* DishRows */}
+          {dishes?.map((dish) => (
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.short_description}
+              price={dish.price}
+              image={dish.image}
+            />
+          ))}
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
