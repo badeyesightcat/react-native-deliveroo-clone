3b38798b0ba85ce357642d0cefe95fb45ef192a0
Beom Mee <whitebigbabybird@gmail.com>
2024-01-21 17:30:14 +0900

    feat: add an animation and progress circle to express an order is ready to be accepted to restaurant

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,10 +1,31 @@
 import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
+import * as Animatable from "react-native-animatable";
+import * as Progress from "react-native-progress";
 
 const PreparingOrderScreen = () => {
   return (
     <SafeAreaView className="bg-[#00ccbb] flex-1 justify-center items-center">
-      <Text>PreparingOrderScreen</Text>
+      <Animatable.Image
+        source={require("../assets/placingOrderLoader.gif")}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-96 w-96"
+      />
+
+      <Animatable.Text
+        animation="slideInUp"
+        iterationCount={1}
+        className="text-lg my-10 text-white font-bold text-center"
+      >
+        Waiting for the restaurnt to accept your order!
+      </Animatable.Text>
+
+      <Progress.Circle
+        size={60}
+        indeterminate={true}
+        color="white"
+      />
     </SafeAreaView>
   );
 };
